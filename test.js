75cd4e7d8b67af659e426c280c7ed4bb4c114abc
pushdown99 <haeyun@gmail.com>
2021-11-18 11:58:23 +0900

    make it better

--- test.js
+++
@@ -1,8 +0,0 @@
-var docxConverter = require('docx-pdf');
-
-docxConverter('./output.docx','./output.pdf',function(err,result){
-  if(err){
-    console.log(err);
-  }
-  console.log('result'+result);
-});
