33f9cde1cc4ce88fec9f00007f4d543a2344aeab
unknown <haeyun@gmail.com>
2021-11-21 12:30:44 +0900

    make it better

--- public/js/kalsa.js
+++ public/js/kalsa.js
@@ -82,7 +82,7 @@ $('#save').on('click', function(event) {
   console.log(JSON.stringify(params));
 
   $.postJSON('/json/register', params).then(res => {
-    viewReceipt (`'output-${res.userid}.pdf'`);
+    viewReceipt (`'/output-${res.userid}.pdf'`);
     console.log(res);
   });
 });
